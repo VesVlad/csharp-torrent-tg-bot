c36f27f23811ddb57b8b085cd051d34c4ccec518
Vlad00k <veselov.vladiken@mail.ru>
2023-06-15 19:13:19 +0700

    add categories

--- TorrentBot/Program.cs
+++ TorrentBot/Program.cs
@@ -10,15 +10,14 @@ using Telegram.Bot.Types;
 
 namespace torrent_tg_bot;
 
-// List<string> cat = new List<string> { "Console", "Movies", "Audio", "PC", "TV", "XXX", "Books", "Other" };
 class Program {
     static readonly ITelegramBotClient bot = new TelegramBotClient("5838918727:AAG-XEa4JfuxQev-4KkUkTel60kqTvPZXks");
     static readonly HttpClient client = new HttpClient();
     private static string token;
+    private static Dictionary<string, int>? categories;
     public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
     {
-        // Некоторые действия
-        Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
+        // Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
         if(update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
         {
             var message = update.Message;
@@ -26,75 +25,89 @@ class Program {
             {
                 await botClient.SendTextMessageAsync(message.Chat, "Добро пожаловать на борт, пират!", cancellationToken: cancellationToken);
                 await botClient.SendTextMessageAsync(message.Chat, "/q ВашЗапрос или /q ВашЗапрос [-cat КатегорияФайла] \n" +
-                                                                   "-cat {Console, Movies, Audio, PC, TV, XXX, Books, Other}");
+                                                                   "-cat {Audio, PC, TV, XXX, Books, Other}", cancellationToken: cancellationToken);
             }
             else if (message.Text.ToLower() == "/help")
             {
                 await botClient.SendTextMessageAsync(message.Chat, "/q ВашЗапрос или /q ВашЗапрос [-cat КатегорияФайла] \n" +
-                                                                   "-cat {Console, Movies, Audio, PC, TV, XXX, Books, Other}");
+                                                                   "-cat {Audio, PC, TV, XXX, Books, Other}", cancellationToken: cancellationToken);
             }
             else if (message.Text.StartsWith("/q"))
             {
                 if (message.Text.Replace("/q", "").Trim().Length > 0)
                 {
+                    string q = message.Text;
+                    string catSubstring = "";
                     if (message.Text.Contains("-cat"))
                     {
-                        int indexer = message.Text.LastIndexOf("-cat");
-                        // message.Text.Substring(indexer, message.Text.Length)
-                        // Console.WriteLine();
-                    }
-                    var response = await client.GetAsync(token
-                    .Replace("query", message.Text.Replace("/q", "").Trim()), cancellationToken);
-                    HttpResponseMessage status = response.EnsureSuccessStatusCode();
-                
-                    await botClient.SendTextMessageAsync(message.Chat,
-                        "Ваш запрос обрабатывается, пожалуйста, подождите...");
-                    
-                    if (status.StatusCode != HttpStatusCode.OK)
-                    {
-                        await botClient.SendTextMessageAsync(message.Chat, "Что-то пошло не так, status code:" + status.StatusCode, cancellationToken: cancellationToken);
-                        throw new Exception("Что-то пошло не так, status code:" + status.StatusCode);
-                    }
-                    var responseBody = await response.Content.ReadAsStringAsync(cancellationToken);
-                    Console.WriteLine(responseBody);
-                    XmlDocument xDoc = new XmlDocument();
-                    xDoc.LoadXml(responseBody);
-                    
-                    if (xDoc.DocumentElement.SelectNodes("//item").Count == 0)
-                    {
-                        await botClient.SendTextMessageAsync(message.Chat, $"В Текущем indexer: {"0magnet"} не удалось ничего найти по вашему запросу: " +
-                                                                           $" {message.Text.Replace("/q", "").Trim()}");
-                    }
-                    
-                    var xmlNodesTitle = xDoc.DocumentElement.SelectNodes("//item/title");
-                    var xmlNodesId = xDoc.DocumentElement.SelectNodes("//item/guid");
-                    var xmlNodesDate = xDoc.DocumentElement.SelectNodes("//item/pubDate");
-                    var xmlNodesSize = xDoc.DocumentElement.SelectNodes("//item/size");
-                    int max_rows = xmlNodesTitle.Count > 15 ? 15 : xmlNodesTitle.Count;
-                    
-                    if (xmlNodesTitle != null)
-                        for (int i = 0; i < max_rows; i++)
+                        int catStartIndex = q.LastIndexOf("-cat") + 4;
+                        catSubstring = q.Substring(catStartIndex, q.Length - catStartIndex).Trim();
+                        
+                        if (catSubstring.Trim().Length != 0 && categories.ContainsKey(catSubstring))
                         {
-                            var title = ((XmlElement)xmlNodesTitle[i]).InnerText;
-                            title = title != null && title.Length > 100 ? title.Substring(0, 100) : title;
-                            
-                            var guid = ((XmlElement)xmlNodesId[i]).InnerText;
-                            guid = guid != null && guid.Length > 100 ? guid.Substring(0, 100) : guid;
-                            
-                            var date = ((XmlElement)xmlNodesDate[i]).InnerText;
-                            date = date != null && date.Length > 100 ? date.Substring(0, 100) : date;
-                            
-                            var size = ((XmlElement)xmlNodesSize[i]).InnerText;
-                            float fileSize = float.Parse(size)/ (1024 * 1024 * 1024);
-                            
-                            await botClient.SendTextMessageAsync(message.Chat,
-                                $"{i+1}) Title: «{title}»\n     Url: {guid}\n     PubDate: {date}\n     Size: {fileSize} GB");
+                            q = q.Substring(0, catStartIndex - 4);
                         }
+                        else
+                        {
+                            await botClient.SendTextMessageAsync(message.Chat, "Пустая или несуществующая категория -сat: " + q, cancellationToken: cancellationToken);
+                        }
+                        
+                    }
+                    
+                    var response = await client.GetAsync(token
+                    .Replace("query", q.Replace("/q", "").Trim())  + categories[catSubstring].ToString(), cancellationToken: cancellationToken);
+                    HttpStatusCode status = response.StatusCode;
+                    await botClient.SendTextMessageAsync(message.Chat,
+                        "Ваш запрос обрабатывается, пожалуйста, подождите...", cancellationToken: cancellationToken);
+                    
+                    if (status != HttpStatusCode.OK)
+                    {
+                        await botClient.SendTextMessageAsync(message.Chat, "Что-то пошло не так, status code:" + status, cancellationToken: cancellationToken);
+                    }
+                    else
+                    {
+                        
+                        var responseBody = await response.Content.ReadAsStringAsync(cancellationToken);
+                        XmlDocument xDoc = new XmlDocument();
+                        xDoc.LoadXml(responseBody);
+                    
+                        if (xDoc.DocumentElement.SelectNodes("//item").Count == 0)
+                        {
+                            await botClient.SendTextMessageAsync(message.Chat, $"В Текущем indexer: {"anidex"} не удалось ничего найти по вашему запросу: " +
+                                                                               $" {message.Text.Replace("/q", "").Trim()}", cancellationToken: cancellationToken);
+                        }
+                        var xmlNodesTitle = xDoc.DocumentElement.SelectNodes("//item/title");
+                        var xmlNodesId = xDoc.DocumentElement.SelectNodes("//item/guid");
+                        var xmlNodesDate = xDoc.DocumentElement.SelectNodes("//item/pubDate");
+                        var xmlNodesSize = xDoc.DocumentElement.SelectNodes("//item/size");
+                        int maxRows = xmlNodesTitle.Count > 15 ? 15 : xmlNodesTitle.Count;
+
+                        if (xmlNodesTitle is not null)
+                        {
+                            for (int i = 0; i < maxRows; i++)
+                            {
+                                var title = ((XmlElement)xmlNodesTitle[i]).InnerText;
+                                title = title != null && title.Length > 100 ? title.Substring(0, 100) : title;
+                                
+                                var guid = ((XmlElement)xmlNodesId[i]).InnerText;
+                                guid = guid != null && guid.Length > 100 ? guid.Substring(0, 100) : guid;
+                                
+                                var date = ((XmlElement)xmlNodesDate[i]).InnerText;
+                                date = date != null && date.Length > 100 ? date.Substring(0, 100) : date;
+                                
+                                var size = ((XmlElement)xmlNodesSize[i]).InnerText;
+                                float fileSize = float.Parse(size)/ (1024 * 1024 * 1024);
+                                
+                                await botClient.SendTextMessageAsync(message.Chat,
+                                    $"({i+1}) Title: «{title}»\n     Url: {guid}\n     PubDate: {date}\n     Size: {fileSize} GB", cancellationToken: cancellationToken);
+                            } 
+                        }
+                    }
                 }
                 else
                 {
-                    await botClient.SendTextMessageAsync(message.Chat, "Аргумент запроса пуст.\n Для отправки запроса введите /q ВашЗапрос или /q ВашЗапрос [-cat КатегорияФайла] \n" +
-                    "-cat {Console, Movies, Audio, PC, TV, XXX, Books, Other}");
+                    await botClient.SendTextMessageAsync(message.Chat, "Пустой запрос.\n Для отправки запроса введите /q ВашЗапрос или /q ВашЗапрос [-cat КатегорияФайла] \n" +
+                    "-cat {Audio, PC, TV, XXX, Books, Other}", cancellationToken: cancellationToken);
                 }
                 
             }
@@ -119,8 +132,17 @@ class Program {
 
         var cts = new CancellationTokenSource();
         var cancellationToken = cts.Token;
+        categories = new Dictionary<string, int>()
+        {
+            {"Audio", 3000},
+            {"PC", 4000},
+            {"TV", 5000},
+            {"XXX", 6000},
+            {"Books", 7000},
+            {"Other", 8000}
+        };
         token =
-            "http://localhost:9118/api/v2.0/indexers/0magnet/results/torznab/api?apikey=95pqrsj9c8te7w75baz4gnxqdpf8to4e&t=search&q=query";
+            "http://localhost:9118/api/v2.0/indexers/anidex/results/torznab/api?apikey=95pqrsj9c8te7w75baz4gnxqdpf8to4e&t=search&q=query&cat=";
         var receiverOptions = new ReceiverOptions
         {
             AllowedUpdates = { }, // receive all update types
